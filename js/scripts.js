58622fe34598045c948f3328059c110aa673f7c2
Łukasz Rozwadowski <lukasz.rozwadowski7@gmail.com>
2017-11-18 11:24:24 +0100

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,23 @@
-alert('obliczam pole trójkąta');
-var a = prompt("wpisz wartosc dla a ");
-	h = prompt("wpisz wartość dla h ");
+var a, h;
+var i = 1;
+
+var button = document.getElementById('licz');
+
+button.addEventListener('click', function() {
+  var triangle1Area = getTriangleArea();
+  console.log('Pole trójkąta ' + triangle1Area);
+});
 
 function getTriangleArea (a, h) {
+	alert('obliczam pole trójkąta numer '+ i++);
+
+	a = prompt("wpisz wartosc dla a ");
+	h = prompt("wpisz wartość dla h ");
+
 	if ( ( a <= 0 )  || ( h <= 0 ) ) {
-		console.log("Nieprawidłowe dane");
+    return 'nie mogło zostać obliczone, ponieważ a lub h jest nieprawidłowe';
 	}
 	else {
-	return (a*h/2);
+	  return 'wynosi ' + (a*h/2);
 	}
 }
-console.log(getTriangleArea(a, h));
-
-var 
-	triangle1Area = getTriangleArea(10, 6);
-	triangle2Area = getTriangleArea(12, 8);
-	triangle3Area = getTriangleArea(14, 10);
-
-console.log(triangle1Area),
-console.log(triangle2Area),
-console.log(triangle3Area);
